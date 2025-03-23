fed193a2c3c8e364879bfc2da9ea0d01e1ed23af
Olivia Cameron <olivia.cameronj15@gmail.com>
2025-03-22 20:01:29 -0400

    feature: first commit (scaffolding)

---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from 'tailwindcss'
+const config: Config = {
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                mono: ['var(--montserrat-font)'],
+                sans: ['var(--inter-font)'],
+            },
+            colors: {
+                primary: {
+                    DEFAULT: 'var(--primary)',
+                },
+                secondary: {
+                    DEFAULT: 'var(--secondary)',
+                },
+            },
+        },
+    },
+    plugins: [],
+}
+export default config
