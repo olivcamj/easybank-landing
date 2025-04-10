b01bcfb8dd6a17ea5eba4f8c1de1f540f01d4d6a
Olivia Cameron <olivia.cameronj15@gmail.com>
2025-04-10 12:01:22 -0400

    feature: add images

---
+++ src/components/Hero.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <section>
+            <h1>Next generation digital banking</h1>
+            <p>
+                Take your financial life online. Your Easybank account will be a
+                one-stop-shop for spending, saving, budgeting, investing, and
+                much more.
+            </p>
+            <button>Request Invite</button>
+        </section>
+    )
+}
+
+export default Hero
