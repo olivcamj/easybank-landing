f793b0093808ba506456447fd350e2dfbdf6608f
Olivia Cameron <olivia.cameronj15@gmail.com>
2025-04-15 14:42:34 -0400

    feature: add font from the design

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import './assets/fonts/public-sans/300.css'
+import './assets/fonts/public-sans/400.css'
+import './assets/fonts/public-sans/700.css'
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <App />
+    </StrictMode>
 )
