9878ebdbce7a5b2015997f331bf53df79adbdfc4
Olivia Cameron <olivia.cameronj15@gmail.com>
2025-05-06 15:29:23 -0400

    feat: button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+const Button = () => {
+    return (
+        <>
+            <button className="hover:bg-opacity-90 mt-4 h-[44] w-[163] cursor-pointer rounded-full border-transparent bg-linear-[135deg,#33d35e_0%,#2ab6d9_99.58%] bg-cover bg-no-repeat px-4 py-2 text-sm font-bold text-white transition duration-150 ease-in-out focus:ring-2 focus:outline-none active:scale-[0.98]">
+                Request Invite
+            </button>
+        </>
+    )
+}
+
+export default Button
