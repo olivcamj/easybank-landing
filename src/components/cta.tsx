4124d1837a97d6ee3e198c8beb8960c4428ff25e
Olivia Cameron <olivia.cameronj15@gmail.com>
2025-05-06 15:35:34 -0400

    feat: cta component initial layout

---
+++ src/components/cta.tsx
@@ -0,0 +1,16 @@
+const cta = () => {
+  return (
+      <section>
+          <h1>Why choose Easybank?</h1>
+          <p>
+              We leverage Open Banking to turn your bank account into your
+              financial hub. Control your finances like never before.
+      </p>
+      <div>
+      {/* Placeholder for cards */}
+      </div>
+      </section>
+  )
+}
+
+export default cta
