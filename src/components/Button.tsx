d7fa6556a0fc77e0f2d202f859936faaceabd470
Olivia Cameron <olivia.cameronj15@gmail.com>
2025-06-22 10:03:59 -0400

    fix: clean up btn styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,7 @@
 const Button = () => {
     return (
         <>
-            <button className="hover:bg-opacity-90 mt-4 h-[44] w-[163] cursor-pointer rounded-full border-transparent bg-linear-[135deg,#33d35e_0%,#2ab6d9_99.58%] bg-cover bg-no-repeat px-4 py-2 text-sm font-bold text-white transition duration-150 ease-in-out focus:ring-2 focus:outline-none active:scale-[0.98]">
+            <button className="hover:bg-opacity-90 font-public-sans mt-[20px] cursor-pointer rounded-full border-transparent bg-linear-[135deg,#33d35e_0%,#2ab6d9_99.58%] bg-cover bg-no-repeat px-[39.38px] py-[8px] text-[14px] leading-[28px] font-bold text-[#fff] transition duration-150 ease-in-out focus:outline-none active:scale-[0.98]">
                 Request Invite
             </button>
         </>
