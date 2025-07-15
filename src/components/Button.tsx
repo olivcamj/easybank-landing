53a4000284e748b0bddb203bf9f2169da6e082b3
Olivia Cameron <olivia.cameronj15@gmail.com>
2025-07-15 18:05:01 -0400

    style: btn color is not quite right

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,7 @@
 const Button = () => {
     return (
         <>
-            <button className="hover:bg-opacity-90 font-public-sans cursor-pointer rounded-full border-transparent bg-linear-[135deg,#33d35e_0%,#2ab6d9_99.58%] bg-cover bg-no-repeat px-[39.38px] py-[8px] text-[14px] leading-[28px] font-bold text-[#fff] transition duration-150 ease-in-out focus:outline-none active:scale-[0.98]">
+            <button className="hover:bg-opacity-90 font-public-sans cursor-pointer rounded-full border-transparent bg-linear-[135deg,#33d35e_0.42%,#2ab6d9_99.58%] bg-cover bg-no-repeat px-[39.38px] py-[8px] text-[14px] leading-[28px] font-bold text-[#fff] transition duration-150 ease-in-out focus:outline-none active:scale-[0.98]">
                 Request Invite
             </button>
         </>
