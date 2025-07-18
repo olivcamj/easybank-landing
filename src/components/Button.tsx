bf3855c60b68ca7435634b945edd85b92615e416
Olivia Cameron <olivia.cameronj15@gmail.com>
2025-07-18 10:13:26 -0400

    add: correct hover effect

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,7 @@ const Button = () => {
     return (
         <button
             type="button"
-            className="font-public-sans cursor-pointer rounded-full border-transparent bg-linear-[135deg,#33d35e_0.42%,#2ab6d9_99.58%] bg-cover bg-no-repeat px-10 py-2 text-sm leading-7 font-bold text-white transition duration-110 ease-in-out focus:outline-none active:scale-[0.98] motion-reduce:transition-none"
+            className="font-public-sans cursor-pointer rounded-full border-transparent bg-linear-[135deg,#33d35e_0.42%,#2ab6d9_99.58%] bg-cover bg-no-repeat px-10 py-2 text-sm leading-7 font-bold text-white transition duration-110 ease-in-out hover:brightness-110 focus:outline-none active:scale-[0.98] motion-reduce:transition-none"
         >
             Request Invite
         </button>
